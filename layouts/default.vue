8c5711ed7258b7d0036a2698ea22f78d85fda74d
DESKTOP-5BFI67D\git clone git@gitlab.com:ganjo3/x.gitGamer <mountasser.massine@gmail.com>
2022-01-24 16:06:05 +0100

    :rocket: web portfolio prototype 1.0 :rocket:

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-if="firstp">Bienvenue
+      
+    </div>
+    <div v-else><nuxt /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      firstp: true,
+    };
+  },
+};
+</script>
+
+<style></style>
